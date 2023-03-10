036b9c48bb1f0f410b02fe94999a111b81c83aa6
unknown <irbrown54321@gmail.com>
2023-03-10 13:37:54 -0500

    Added clear button

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const inputField = document.getElementById("gtinInput");
 const outputField = document.getElementById("gtinOutput");
 const formatButton = document.getElementById("formatBtn");
+const clearButton = document.getElementById("clearBtn");
 
 function format(){
     outputField.value = inputField.value.replace(/\n|\s/g,",");
 }
 
+function clearField(){
+    inputField.value = "";
+}
+
 formatButton.addEventListener("click", format);
+clearButton.addEventListener("click", clearField);
